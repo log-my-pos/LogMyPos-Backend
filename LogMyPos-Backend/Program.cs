504d1ab18042cb059f6297ac3ac9161923ff0c2b
The Panda Oliver <Oliver.h.froberg@gmail.com>
2026-04-22 11:26:04 +0200

    Move Swagger configuration to the end of the HTTP request pipeline setup

--- LogMyPos-Backend/Program.cs
+++ LogMyPos-Backend/Program.cs
@@ -14,10 +14,12 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
-	app.MapOpenApi();
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	
 }
+
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.MapControllers();
