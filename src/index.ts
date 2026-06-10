d5d8f438df2dc97ded3784979166c7ab14e0b1e0
Sobbing Cat <sobbingcat@iyagigroup.com>
2026-06-10 13:31:07 +0200

    updated??

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,6 @@ import { authRoutes } from "./routes/auth";
 const app = new Elysia()
   .get("/", () => "Server is up and running")
 
-  .group("/api", (api) => api.use(authRoutes))
+  .group("/api", (api) => api.use(authRoutes));
 
-  .listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-);
+export default app;
