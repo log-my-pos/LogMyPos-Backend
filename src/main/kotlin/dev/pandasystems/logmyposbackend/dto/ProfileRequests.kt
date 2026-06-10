3ac65de09c1dc120c6505d3169466b53f0e65238
Sobbing Cat <sobbingcat@iyagigroup.com>
2026-06-10 11:40:42 +0200

    Initial commit (via bun create)

--- src/main/kotlin/dev/pandasystems/logmyposbackend/dto/ProfileRequests.kt
+++
@@ -1,9 +0,0 @@
-package dev.pandasystems.logmyposbackend.dto
-
-data class ProfileCreateRequest(
-	val displayName: String,
-)
-
-data class ProfileUpdateRequest(
-	val displayName: String? = null,
-)
